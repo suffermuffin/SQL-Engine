35bf7b79d2eb3a142be8035e091b6eb943f571aa
suffermuffin <dantsymbal@hotmail.com>
2026-05-11 01:39:51 +0200

    Add tests for unmanadged connections

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,6 @@
 import unittest
 
+import sqlite3
 import os
 import logging
 import sys
@@ -10,7 +11,7 @@ from sqlengine       import sqlgen as sql
 from sqlengine.utils import shared_connection
 
 from src.utils  import format_logging, download_file, CHINOOK_URL
-from src.tables import Employees, Coordinates, Point, coord_schema, coords_data, employees_data
+from src.tables import Employees, Coordinates, Point, coord_schema, COORDS_DATA, EMPLOYEES_DATA
 
 
 TEST_DIR   = "temp/"
@@ -20,6 +21,7 @@ TEST_DB    = "temp/test.db"
 _TEARDOWM = False
 
 format_logging("INFO")
+logging.getLogger("sqlengine").setLevel(logging.CRITICAL)
 
 logger = logging.getLogger(__name__)
 
@@ -72,15 +74,24 @@ class TestSqlTable(unittest.TestCase):
 
     
     def tearDown(self) -> None:
+        
+        self.assertFalse(self.coord_table.in_transaction())
+        self.assertFalse(self.empl_table.in_transaction())
+        self.assertFalse(self.coord_table_s.in_transaction())
+
+        self.coord_table.close_connection()
+        self.empl_table.close_connection()
+        self.coord_table_s.close_connection()
+
         self.coord_table.drop_table(True)
         self.empl_table.drop_table(True)
     
     
     def test_getitem_single_primary(self):
         
-        self.coord_table.insert_many(coords_data)
+        self.coord_table.insert_many(COORDS_DATA)
 
-        for row_or, row_re in zip(coords_data[:5], self.coord_table[:5]):
+        for row_or, row_re in zip(COORDS_DATA[:5], self.coord_table[:5]):
             self.assertEqual(row_or[0], row_re[0])
             self.assertEqual(row_or[1], row_re[1])
             self.assertIsInstance(row_re[2], Point)
@@ -92,9 +103,9 @@ class TestSqlTable(unittest.TestCase):
     
     def test_getitem_double_primary(self):
         
-        self.empl_table.insert_many(employees_data)
+        self.empl_table.insert_many(EMPLOYEES_DATA)
 
-        for data_or in employees_data:
+        for data_or in EMPLOYEES_DATA:
             key_1, key_2 = data_or[:2]
             data_re = self.empl_table[[key_1, key_2]]
             self.assertEqual(len(data_or), len(data_re))
@@ -140,8 +151,8 @@ class TestSqlTable(unittest.TestCase):
         with shared_connection(self.coord_table, self.empl_table, self.coord_table_s, **self.coord_table.connection_params):
             self.coord_table_s.create_table()
 
-            self.coord_table.insert_many(coords_data)
-            self.empl_table.insert_many(employees_data)
+            self.coord_table.insert_many(COORDS_DATA)
+            self.empl_table.insert_many(EMPLOYEES_DATA)
 
             for row in self.coord_table:
                 self.coord_table_s.insert(*row)
@@ -155,8 +166,8 @@ class TestSqlTable(unittest.TestCase):
         step_1_shapes = (self.coord_table.shape, self.empl_table.shape)
 
         with shared_connection(self.coord_table, self.empl_table, **params):
-            self.coord_table.insert_many(coords_data)
-            self.empl_table.insert_many(employees_data)
+            self.coord_table.insert_many(COORDS_DATA)
+            self.empl_table.insert_many(EMPLOYEES_DATA)
             step_2_shapes = (self.coord_table.shape, self.empl_table.shape)
         
         self.assertEqual(step_2_shapes, (self.coord_table.shape, self.empl_table.shape))
@@ -168,7 +179,7 @@ class TestSqlTable(unittest.TestCase):
         step_1_coord_shape = self.coord_table.shape
 
         with self.coord_table.transaction():
-            self.coord_table.insert_many(coords_data)
+            self.coord_table.insert_many(COORDS_DATA)
             step_2_coord_shape = self.coord_table.shape
 
         self.assertEqual(step_2_coord_shape, self.coord_table.shape)
@@ -180,7 +191,7 @@ class TestSqlTable(unittest.TestCase):
         step_1_empl_shape = self.empl_table.shape
 
         with self.empl_table.transaction():
-            self.empl_table.insert_many(employees_data)
+            self.empl_table.insert_many(EMPLOYEES_DATA)
             step_2_empl_shape = self.empl_table.shape
 
         self.assertEqual(step_2_empl_shape, self.empl_table.shape)
@@ -191,8 +202,8 @@ class TestSqlTable(unittest.TestCase):
         
         params = self.coord_table.connection_params
 
-        coords_data_1, coords_data_2 = coords_data[:len(coords_data)//2], coords_data[len(coords_data)//2:]
-        employees_data_1, employees_data_2 = employees_data[:len(employees_data)//2], employees_data[len(employees_data)//2:]
+        coords_data_1, coords_data_2 = COORDS_DATA[:len(COORDS_DATA)//2], COORDS_DATA[len(COORDS_DATA)//2:]
+        employees_data_1, employees_data_2 = EMPLOYEES_DATA[:len(EMPLOYEES_DATA)//2], EMPLOYEES_DATA[len(EMPLOYEES_DATA)//2:]
 
         self.coord_table.insert_many(coords_data_1)
         self.empl_table.insert_many(employees_data_1)
@@ -219,7 +230,7 @@ class TestSqlTable(unittest.TestCase):
 
     def test_transaction_rollback_coords(self):
         
-        coords_data_1, coords_data_2 = coords_data[:len(coords_data)//2], coords_data[len(coords_data)//2:]
+        coords_data_1, coords_data_2 = COORDS_DATA[:len(COORDS_DATA)//2], COORDS_DATA[len(COORDS_DATA)//2:]
         self.coord_table.insert_many(coords_data_1)
         step_1_shape = self.coord_table.shape
 
@@ -235,7 +246,7 @@ class TestSqlTable(unittest.TestCase):
     
     def test_transaction_rollback_empl(self):
         
-        employees_data_1, employees_data_2 = employees_data[:len(employees_data)//2], employees_data[len(employees_data)//2:]
+        employees_data_1, employees_data_2 = EMPLOYEES_DATA[:len(EMPLOYEES_DATA)//2], EMPLOYEES_DATA[len(EMPLOYEES_DATA)//2:]
 
         self.empl_table.insert_many(employees_data_1)
         step_1_shape = self.empl_table.shape
@@ -267,6 +278,158 @@ class TestSqlTable(unittest.TestCase):
                 copy_biggest_table.insert_many(batch)
             
             self.assertEqual(biggest_table.shape, copy_biggest_table.shape)
+
+    
+    def test_unmanaged_connection_iteration(self):
+        
+        self.coord_table.open_connection()
+        self.coord_table.insert_many(COORDS_DATA)
+        
+        for idx, _, _, temp in self.coord_table:
+            self.coord_table.update(f"ID = {idx}", {"temp" : temp*2})
+
+        self.coord_table.commit()
+        self.coord_table.close_connection()
+
+        rows = self.coord_table.select()
+
+        self.assertEqual(len(rows), len(COORDS_DATA))
+
+        for row_or, row_re in zip(COORDS_DATA, rows):
+            self.assertEqual(row_or[-1]*2, row_re[-1])
+
+    
+    def test_unmanaged_connection_rollback(self):
+        
+        self.coord_table.open_connection()
+        self.coord_table.insert_many(COORDS_DATA)
+
+        assert len(COORDS_DATA) > 0, "No coords data"
+
+        self.assertEqual(len(self.coord_table), len(COORDS_DATA))
+
+        self.coord_table.rollback()
+        self.coord_table.close_connection()
+
+        self.assertEqual(len(self.coord_table), 0)
+
+
+    def test_unmanaged_connection_attrs(self):
+        with self.assertRaises(RuntimeError):
+            self.empl_table.tx_cursor
+
+        with self.assertRaises(RuntimeError):
+            self.empl_table.tx_conn
+
+        self.empl_table.open_connection()
+
+        conn = self.empl_table.tx_conn
+        self.assertIsInstance(conn, sqlite3.Connection)
+
+        curs = self.empl_table.tx_cursor
+        self.assertIsInstance(curs, sqlite3.Cursor)
+
+        self.empl_table.close_connection()
+
+        with self.assertRaises(RuntimeError):
+            self.empl_table.tx_cursor
+
+        with self.assertRaises(RuntimeError):
+            self.empl_table.tx_conn
+
+    
+    def test_unmanaged_attr_manip_edge_case(self):
+
+        self.empl_table.open_connection()
+        conn = self.empl_table.tx_conn
+        conn.close()
+
+        with self.assertRaises(sqlite3.ProgrammingError):
+            self.empl_table.close_connection()
+
+        # additional teardown
+        delattr(self.empl_table, "_trans_cursor")
+        delattr(self.empl_table, "_trans")
+
+    
+    def test_transaction_attr_manip_edge_case(self):
+
+        with self.assertRaises(sqlite3.ProgrammingError):
+            with self.empl_table.transaction():
+                self.empl_table.tx_conn.close()
+
+        # additional teardown
+        delattr(self.empl_table, "_trans_cursor")
+        delattr(self.empl_table, "_trans")
+
+
+    def test_transaction_edge_case(self):
+        
+        with self.assertRaises(RuntimeError):
+            with self.empl_table.transaction():
+                self.empl_table.close_connection()
+
+        with self.assertRaises(RuntimeError):
+            with self.empl_table.transaction():
+                self.empl_table.open_connection()
+
+    
+    def test_shared_connection_edge_case(self):
+        
+        with self.assertRaises(RuntimeError):
+            with shared_connection(self.coord_table, self.empl_table, **self.coord_table.connection_params):
+                self.coord_table.close_connection()
+
+        with self.assertRaises(RuntimeError):
+            with shared_connection(self.coord_table, self.empl_table, **self.coord_table.connection_params):
+                self.empl_table.open_connection()
+
+
+    def test_transaction_manual_commit(self):
+
+        with self.coord_table.transaction(autocommit=False):
+            self.coord_table.insert_many(COORDS_DATA)
+
+        self.assertEqual(len(self.coord_table), 0)
+
+        with self.coord_table.transaction(autocommit=False):
+            self.coord_table.insert_many(COORDS_DATA)
+            self.coord_table.commit()
+
+        self.assertEqual(len(self.coord_table), len(COORDS_DATA))
+
+
+    def test_shared_connection_manual_commit(self):
+        
+        with shared_connection(self.coord_table, self.empl_table, autocommit=False):
+            self.coord_table.insert_many(COORDS_DATA)
+            self.empl_table.insert_many(EMPLOYEES_DATA)
+
+        self.assertEqual(len(self.coord_table), 0)
+        self.assertEqual(len(self.empl_table), 0)
+
+        with shared_connection(self.coord_table, self.empl_table, autocommit=False):
+            self.coord_table.insert_many(COORDS_DATA)
+            self.empl_table.insert_many(EMPLOYEES_DATA)
+            self.coord_table.commit()
+            self.empl_table.commit()            
+
+        self.assertEqual(len(self.coord_table), len(COORDS_DATA))
+        self.assertEqual(len(self.empl_table), len(EMPLOYEES_DATA))
+
+
+    def test_shared_connection_with_one_table(self):
+        with shared_connection(self.coord_table):
+            self.coord_table.tx_conn
+            self.coord_table.tx_cursor
+
+            self.coord_table.insert_many(COORDS_DATA)
+
+            for idx, _, _, temp in self.coord_table:
+                self.coord_table.update(f"ID = {idx}", {"temp" : temp*2})
+
+        self.assertEqual(len(self.coord_table), len(COORDS_DATA))
+
 
 
 if __name__ == '__main__':
